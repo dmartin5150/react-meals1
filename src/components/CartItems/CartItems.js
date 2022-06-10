f835d57ad0cccb131c056790e5c5bf73f69ed464
David <dmartin5150@icloud.com>
2022-06-09 22:33:37 -0500

    minor changes

--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -4,7 +4,6 @@ import Card from "../UI/Card/Card";
 import CartItem from "./CartItem";
 
 const CartItems = (props) => {
-
   const orderCartHandler = (event) => {
     event.preventDefault();
     console.log("Placing Order");
@@ -31,13 +30,12 @@ const CartItems = (props) => {
       </ul>
       <div className={classes["cart-items__total"]}>
         {props.filteredMeals.length === 0 && <p>No Items in Cart</p>}
-        {props.filteredMeals.length !== 0 && 
-        <React.Fragment>
-        <p>Total Amount</p> 
-        <p>${props.total}</p> 
-        </React.Fragment>
-        }
-
+        {props.filteredMeals.length !== 0 && (
+          <React.Fragment>
+            <p>Total Amount</p>
+            <p>${props.total}</p>
+          </React.Fragment>
+        )}
       </div>
       <div className={classes["cart-items__actions"]}>
         <button
